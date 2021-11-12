d7e7f0008df44fdf488642d62cbf529a7821834f
teuntjepeeters <44974442+teuntjepeeters@users.noreply.github.com>
2021-11-12 10:35:20 +0100

    Add files via upload

---
+++ Afvinkopdracht3.py
@@ -0,0 +1,15 @@
+# importeer de twee dictionaries vanuit de translation.py
+from translation import code
+from translation import aa3
+
+
+def main():
+    print(code)
+    print(aa3)
+
+    # Gebruik onderstaande DNA sequentie voor het uitvoeren van
+    # opdracht 2 en 3
+    dna = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAA\
+    TAAGCCTGAATGATCCGAGTAGCATCTCAG"
+
+main()
